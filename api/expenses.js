b22687bb19d9e47698ad21387f1ce1dc1086c39b
Ogust11 <devosaugustin@gmail.com>
2025-12-03 11:33:11 +0100

    Create expenses.js

---
+++ api/expenses.js
@@ -0,0 +1,16 @@
+{
+  "name": "vercel-expense-tracker-api",
+  "version": "1.0.0",
+  "description": "Serverless API for expense tracking on Vercel.",
+  "main": "api/expenses.js",
+  "scripts": {
+    "start": "vercel dev"
+  },
+  "keywords": [
+    "vercel",
+    "serverless",
+    "expense"
+  ],
+  "author": "",
+  "license": "ISC"
+}
